3bb65eabed861937592510c10f6e44ef28c2b754
wonjae jun <woncha7764@gmail.com>
2022-03-22 17:13:42 +0900

    dropdown prac

---
+++ clone1/README.md
@@ -0,0 +1,5 @@
+## Dropdown-CloneCoding
+
+### 참조 Url: https://www.youtube.com/watch?v=uUT1yMKCWcQ
+
+Dropdown을 만들어보다가 막히는 부분이 있어서 유튜브에서 Vanilla.js로 dropdown 만드는 것을 따라해보았다.
---
+++ clone1/index.js
@@ -0,0 +1,101 @@
+const list = document.getElementById("todos-list");
+const addBtn = document.getElementById("todo-add-btn");
+const addInput = document.getElementById("todo-input");
+
+function createTodo() {
+  let text = addInput.value;
+
+  if(text ===""){
+    return;
+  }
+
+  let li = document.createElement("li")
+  let checkbox = document.createElement("input");
+  checkbox.classList.add("checkbox");
+  checkbox.type = "checkbox";
+
+  let paragraph = document.createElement("p")
+  paragraph.classList.add("paragraph");
+  paragraph.textContent=text;
+
+  let remove = document.createElement("span")
+  remove.classList.add("remove");
+  remove.innerHTML = "&cross;"
+
+  li.appendChild(checkbox)
+  li.appendChild(paragraph)
+  li.appendChild(remove)
+  list.appendChild (li)
+  addInput.value=""
+}
+
+function showEditInput(paragraphElement){
+
+  let editInput = document.getElementsByName("editInput")[0]
+  if(editInput){
+    editInput.remove();
+  }
+
+  let input = document.createElement("input")
+  input.type="text"
+  input.name = "editInput"
+  input.value=paragraphElement.textContent;
+  input.classList.add("editInput")
+
+  paragraphElement.parentElement.appendChild(input)
+  input.focus();
+}
+
+function removeTodo(removeElement){
+  removeElement.parentElement.remove()
+}
+
+function toggleComplete(inputElement){
+  
+  if(inputElement.checked===false){
+    inputElement.parentElement.classList.remove("complete")
+  }else{
+    inputElement.parentElement.classList.add("complete")
+  }
+}
+
+function updateTodo(){
+  let editInput = document.getElementsByName("editInput")[0];
+  console.log(editInput)
+  if(!editInput){
+    return
+  }
+
+  const newText = editInput.value
+
+  if(newText !==""){
+    let paragraph = editInput.parentElement.querySelector(".paragraph");
+    paragraph.textContent = newText
+  }
+  editInput.remove()
+}
+
+// addEventListener
+list.addEventListener("click", (e)=>{
+
+  e.stopPropagation()
+
+  switch (e.target.tagName){
+    case "P":
+        showEditInput(e.target);
+        break;
+    case "SPAN":
+        removeTodo(e.target);
+        break;
+  }
+})
+
+list.addEventListener("change", (e)=>(e.target.tagName==="INPUT" && e.target.type ==="checkbox")&&toggleComplete(e.target))
+
+list.addEventListener("keypress",  (e)=>(e.target.tagName==="INPUT" && e.target.type ==="text"&&e.key==="Enter")&&updateTodo(e.target)  )
+document.addEventListener("click", updateTodo)
+addBtn.addEventListener("click", (e)=>{
+  e.stopPropagation()
+  createTodo();
+} )
+addInput.addEventListener("keypress",(e)=>e.key==="Enter"&&createTodo())
